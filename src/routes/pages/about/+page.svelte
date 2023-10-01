39e8f415c396dbcbf0ca010966ece107f5f9359f
a-nguyen-kangan <a.nguyen@kangan.edu.au>
2023-10-02 10:02:06 +1100

    updated about page

--- src/routes/pages/about/+page.svelte
+++ src/routes/pages/about/+page.svelte
@@ -1,4 +1,36 @@
 <script>
 
 </script>
-<p>About page</p>
+<div>
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Topics/Tools/Stuff we're going to be using</h1>
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white">Rough Timeline</h1>
+    <h2 class="text-4xl font-extrabold dark:text-white">Weeks 1-4: Basic Web</h2>
+    <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>CSS Framework</li>
+        <li>Git</li>
+        <li>GitHub Pages</li>
+    </ul>
+    <br>
+    <hr>
+    <br>
+    <h2 class="text-4xl font-extrabold dark:text-white">Weeks 5-8: Svelte</h2>
+    <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li>Components</li>
+        <li>Props</li>
+        <li>Logic</li>
+        <li>Events</li>
+        <li>Bindings</li>
+    </ul>
+    <br>
+    <hr>
+    <br>
+    <h2 class="text-4xl font-extrabold dark:text-white">Weeks 9-12: Data Stores</h2>
+    <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li>Svelte Lifecycle</li>
+        <li>Supabase Datastore</li>
+        <li>Supabase CRUD</li>
+    </ul>
+</div>
